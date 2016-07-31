14a998b1ccd1f47f510d9800439c20499cf5cf91
Filipe Pires <fdmpires@gmail.com>
2016-07-31 15:18:21 +0100

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Starting Point</title>
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+    <h1>Starting Point to php project</h1>
+
+
+    <style src="public/js/script.js"></style>
+</body>
+</html>
